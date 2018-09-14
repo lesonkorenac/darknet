e8f75e4e8cc4fb429ec840ab231e13d519da2ed9
Jan Peldřimovský <jan.peldrimovsky@quantasoft.com>
2018-09-14 16:35:39 +0200

    Use OpenCV by default

--- src/yolo_v2_class.hpp
+++ src/yolo_v2_class.hpp
@@ -13,6 +13,10 @@
 #endif
 #endif
 
+#ifndef OPENCV
+#define OPENCV
+#endif
+
 struct bbox_t {
     unsigned int x, y, w, h;    // (x,y) - top-left corner, (w, h) - width & height of bounded box
     float prob;                    // confidence - probability that the object was found correctly
